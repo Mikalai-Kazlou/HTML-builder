2c5d373bd38cfed606d389ecd5944b908776ffa1
Mikalai Kazlou <mikalai_kazlou@epam.com>
2022-11-03 22:52:28 +0300

    feat: add "copy directory" task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,59 @@
+const fs = require('fs');
 
+const path = require('path');
+const dirSource = path.join(__dirname, 'files');
+const dirDestination = path.join(__dirname, 'files-copy');
+
+function deleteFiles(error, files) {
+  if (error)
+    console.log(error);
+  else {
+    files.forEach(file => {
+      if (file.isFile()) {
+        fs.unlink(path.join(dirDestination, file.name),
+          (error) => {
+            if (error) {
+              console.log(error);
+            }
+          });
+      }
+    });
+  }
+}
+
+function copyFiles() {
+  fs.readdir(dirSource, { withFileTypes: true },
+    (error, files) => {
+      if (error)
+        console.log(error);
+      else {
+        files.forEach(file => {
+          if (file.isFile()) {
+            fs.copyFile(path.join(dirSource, file.name), path.join(dirDestination, file.name),
+              (error) => {
+                if (error) {
+                  console.log(error);
+                }
+              });
+          }
+        })
+      }
+    });
+}
+
+function copyDir() {
+  fs.mkdir(dirDestination, { recursive: true },
+    (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+
+  fs.readdir(dirDestination, { withFileTypes: true },
+    (error, files) => {
+      deleteFiles(error, files);
+      copyFiles();
+    });
+}
+
+copyDir();
