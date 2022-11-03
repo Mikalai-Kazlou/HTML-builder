bc0fc26889da2fbe968eacfcbbbb70773fd4c0ce
Mikalai Kazlou <mikalai_kazlou@epam.com>
2022-11-03 21:48:14 +0300

    feat: add "files in folder" task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const path = require('path');
+const dirPath = path.join(__dirname, 'secret-folder');
+
+function showFileInfo(file) {
+  fs.stat(path.join(dirPath, file.name),
+    (error, stats) => {
+      if (error)
+        console.log(error);
+      else {
+        const info = path.parse(file.name);
+        console.log(`${info.name} - ${info.ext.slice(1)} - ${stats.size}b`);
+      }
+    });
+}
+
+fs.readdir(dirPath, { withFileTypes: true },
+  (error, files) => {
+    if (error)
+      console.log(error);
+    else {
+      files.forEach(file => {
+        if (file.isFile()) {
+          showFileInfo(file);
+        }
+      })
+    }
+  })
