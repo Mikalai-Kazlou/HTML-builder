f8a69061fdb3e6a8bcf9c9d90eda10cda83389ba
Mikalai Kazlou <mikalai_kazlou@epam.com>
2022-11-06 08:35:32 +0300

    refactor: refactoring of task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 
 const path = require('path');
-const dirSource = path.join(__dirname, 'files');
-const dirDestination = path.join(__dirname, 'files-copy');
+const sDirSource = path.join(__dirname, 'files');
+const sDirDestination = path.join(__dirname, 'files-copy');
 
-function deleteFiles(error, files) {
+function deleteFiles(destination, error, files) {
   if (error)
     console.log(error);
   else {
     files.forEach(file => {
       if (file.isFile()) {
-        fs.unlink(path.join(dirDestination, file.name),
+        fs.unlink(path.join(destination, file.name),
           (error) => {
             if (error) {
               console.log(error);
@@ -21,15 +21,15 @@ function deleteFiles(error, files) {
   }
 }
 
-function copyFiles() {
-  fs.readdir(dirSource, { withFileTypes: true },
+function copyFiles(source, destination) {
+  fs.readdir(source, { withFileTypes: true },
     (error, files) => {
       if (error)
         console.log(error);
       else {
         files.forEach(file => {
           if (file.isFile()) {
-            fs.copyFile(path.join(dirSource, file.name), path.join(dirDestination, file.name),
+            fs.copyFile(path.join(source, file.name), path.join(destination, file.name),
               (error) => {
                 if (error) {
                   console.log(error);
@@ -41,19 +41,19 @@ function copyFiles() {
     });
 }
 
-function copyDir() {
-  fs.mkdir(dirDestination, { recursive: true },
+function copyDir(source, destination) {
+  fs.mkdir(destination, { recursive: true },
     (error) => {
       if (error) {
         console.log(error);
       }
     });
 
-  fs.readdir(dirDestination, { withFileTypes: true },
+  fs.readdir(destination, { withFileTypes: true },
     (error, files) => {
-      deleteFiles(error, files);
-      copyFiles();
+      deleteFiles(destination, error, files);
+      copyFiles(source, destination);
     });
 }
 
-copyDir();
+copyDir(sDirSource, sDirDestination);
